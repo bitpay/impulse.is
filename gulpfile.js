4d4f14a2104847f4d813f2b787fc3637cdca47bd
Eric Martindale <eric@ericmartindale.com>
2015-01-14 15:23:36 -0500

    Set up package.json appropriately.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
-var gulp = require('gulp');
-var sass 		= require('gulp-sass');
-var watch		= require('gulp-watch');
+var gulp   = require('gulp');
+var sass   = require('gulp-sass');
+var watch  = require('gulp-watch');
+var deploy = require('gulp-gh-pages');
 
 gulp.task('default', function() {
-	gulp.src('./public/sass/*')
-		.pipe(sass())
-		.pipe(gulp.dest('./public/css'))
+  gulp.src('./public/sass/*')
+	  .pipe(sass())
+	  .pipe(gulp.dest('./public/css'));
+});
+
+/**
+ * Push build to gh-pages
+ */
+gulp.task('deploy', function () {
+  return gulp.src('./dist/**/*').pipe( deploy() );
 });
